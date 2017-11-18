7de6785e2bd189df2f9af498c85476f4dd562aa3
slavaerto1 <33775498+slavaerto1@users.noreply.github.com>
2017-11-18 12:13:17 +0500

    Add files via upload

---
+++ злые птицы/AngryBirdsTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+        
+		//  Ниже — это XML документация, её использует ваша среда разработки, 
+		// чтобы показывать подсказки по использованию методов. 
+		// Но писать её естественно не обязательно.
+		/// <param name="v">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public static double FindSightAngle(double v, double distance)
+		{
+            
+
+
+            
+            double g = 9.8;
+            var l = distance;
+
+        
+
+
+            
+            var sin2a = (distance * g) / (v * v);
+            return Math.Asin(sin2a) / 2;
+            return Math.PI / 4;
+
+        }
+	}
+}
